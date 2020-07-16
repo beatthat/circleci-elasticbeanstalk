c08761aaa2aa1f44cd6180ba7f24e5f0e0ab9d43
larry kirschner <larrykirschner@gmail.com>
2020-07-16 16:47:52 -0700

    improvement: can find passive eb env

---
+++ ebs-tools/bin/eb_utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+
+eb_utils::env_find_passive() {
+  local eb_args="${1}"
+  local envs_all="$(eb list ${eb_args})"
+  local envs_all=${envs_all/\* /}
+  local env_result=''
+  for x in ${envs_all}; do
+    local status=$(eb status ${x} ${eb_args})
+    if [[ "${status}" == *""* ]]; then
+      local env_result="${x}"
+      break
+    fi
+  done
+  echo "${env_result}"
+}
